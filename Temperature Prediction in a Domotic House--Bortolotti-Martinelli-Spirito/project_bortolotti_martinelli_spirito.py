e9bd3c5bd1cda037b001f06cfb39bce8a2984795
GiuliaSpirito <giugispi@hotmail.it>
2018-02-19 21:38:35 +0100

    Adding data driven files - Spirito

--- Temperature Prediction in a Domotic House--Bortolotti-Martinelli-Spirito/project_bortolotti_martinelli_spirito.py
+++ Temperature Prediction in a Domotic House--Bortolotti-Martinelli-Spirito/project_bortolotti_martinelli_spirito.py
@@ -7,14 +7,14 @@
 # This dataset is collected from a monitor system settled in a domotic house near Valencia, built by Cardinal Herrera University. It corresponds to approximately 40 days of monitoring data, which are sampled every 15 minutes.
 # The first aim of the project is to investigate the correlation between outside and inside parameters. Then we proceed with the building of a model in order to predict the indoor temperature variation as a function of all the other data which are related to it.
 
-# In[3]:
+# In[1]:
 
 #importing dataset 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-DataFolderPath = "C:\Users\giugi\Dropbox\Building systems group project\Project\DataDriven"
+DataFolderPath = "C:\Users\giugi\Dropbox\Building systems group project\DataDrivenAnalysis\Temperature Prediction in a Domotic House--Bortolotti-Martinelli-Spirito"
 FileName= "NEW-DATA-1.T15.txt"
 FilePath = DataFolderPath+"\\"+FileName
 
@@ -24,7 +24,7 @@ DataFrame["period"] = DataFrame["1:Date"].map(str) + " " + DataFrame["2:Time"].m
 DataFrame.index = DataFrame["period"]
 
 
-# In[4]:
+# In[2]:
 
 # convert indexes from dtype 'object' to 'datetime'
 previousIndex = DataFrame.index 
@@ -32,14 +32,14 @@ ParsedIndex = pd.to_datetime(previousIndex)
 DataFrame.index = ParsedIndex
 
 
-# In[5]:
+# In[3]:
 
 # selecting a specific time interval and removing the NaNs
-DF_myChosenDates = DataFrame["2012-03-20 12:00:00 ":"2012-03-23 12:00:00 "]
+DF_myChosenDates = DataFrame["2012-03-20 12:00:00 ":"2012-03-26 12:00:00 "]
 DF_cleaned = DF_myChosenDates.dropna()
 
 
-# In[6]:
+# In[4]:
 
 # looking for a correlation among data 
 DF_cleaned.corr()
@@ -47,7 +47,7 @@ DF_cleaned.corr()
 
 # Analyzing the data, we decide to futher investigate the correlation among indoor temperature, outdoor temperature, solar irradiance (measured by a piranometer) and inside lighting
 
-# In[7]:
+# In[5]:
 
 # building a normalized dataframe and adding subplots in order to show the correlation graphically 
 Normalized_DF=DF_cleaned.copy()
@@ -76,7 +76,9 @@ plt.show()
 
 # From this graph we can observe that there is a delay between solar irradiance and both temperatures
 
-# In[8]:
+# Now we define some functions useful for the analysis
+
+# In[6]:
 
 # defining a function which calculates the lag, generate columns with a delay of time and save them in a new dataframe
 DF_FinalDataSet= DF_cleaned.copy()
@@ -92,9 +94,40 @@ DF_FinalDataSet= lag_column(DF_FinalDataSet,"4:Temperature_Habitacion_Sensor",24
 DF_FinalDataSet.dropna(inplace=True)
 
 
+# In[7]:
+
+#definition of function determining if the day is a weekend day 
+def weekendDetector(day):
+    weekendLabel=0
+    if (day== 5or day==6):
+        weekendLabel=1
+    else:
+        weekendLabel=0
+    return weekendLabel   #if I get 1 it's a weekend day
+  
+#definition of function determining if it's day or night     
+def dayDetector(hour):
+    dayLabel=1
+    if(hour<20 and hour>9):
+        dayLabel=1
+    else:
+        dayLabel=0
+    return dayLabel      #if I get 1 it's day (from 9 to 20)
+
+
+# Now we add the corresponding columns to dataframe
+
+# In[ ]:
+
+DF_FinalDataSet["weekend"] = [weekendDetector(thisDay) for thisDay in DF_FinalDataSet.index.dayofweek]   
+DF_FinalDataSet["day_night"] = [dayDetector(thisHour) for thisHour in DF_FinalDataSet.index.hour] 
+DF_FinalDataSet.head()  
+DF_FinalDataSet.dropna(inplace=True)   
+
+
 # We apply the shift to the indoor temperature, because it is the parameter which presents a delay w.r.t. the solar irradiance 
 
-# In[9]:
+# In[ ]:
 
 # removing the columns which we do not include in the model 
 DF_FinalDataSet.drop(DF_FinalDataSet.columns[[0,1,6,8,9,10,11,12,13,14,15,17,18,19,23]],axis=1,inplace=True);
@@ -113,14 +146,14 @@ X_train, X_test, Y_train, Y_test = train_test_split(DF_features,DF_target,test_s
 
 # The test_size represents the percentage of data used for testing, in our case 20% for testing while 80% for checking
 
-# In[10]:
+# In[ ]:
 
 # testing the model
 from sklearn import linear_model
 linear_reg = linear_model.LinearRegression()
 
 
-# In[11]:
+# In[ ]:
 
 # defining the linear model with training data (20%)
 linear_reg.fit(X_train,Y_train)
@@ -132,7 +165,7 @@ predict_linearReg_split = linear_reg.predict(X_test)
 Y_test.index
 predict_DF_linearReg_split = pd.DataFrame(predict_linearReg_split,index = Y_test.index,columns = ["TempPredict_LinReg"])
 predict_DF_linearReg_split = predict_DF_linearReg_split.join(Y_test)
-predict_DF_linearReg_split_ChosenDates = predict_DF_linearReg_split["2012-03-20":"2012-03-23"]
+predict_DF_linearReg_split_ChosenDates = predict_DF_linearReg_split["2012-03-20":"2012-03-26"]
 
 predict_DF_linearReg_split_ChosenDates.plot()
 plt.xlabel("time")
@@ -141,7 +174,7 @@ plt.title("Linear Regression Prediction")
 plt.show()
 
 
-# In[12]:
+# In[ ]:
 
 # finding the metrics related to linear regression
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
@@ -152,14 +185,14 @@ R2_score_linearReg_split = r2_score(Y_test,predict_linearReg_split)
 print 'Coefficient of determination (R-squared) = '+str(R2_score_linearReg_split)
 
 
-# In[13]:
+# In[ ]:
 
 # using another method called cross validation
 from sklearn.model_selection import cross_val_predict
 predict_linearReg_CV = cross_val_predict(linear_reg,DF_features,DF_target,cv=10)
 predict_DF_linearReg_CV = pd.DataFrame(predict_linearReg_CV,index = DF_target.index,columns = ["TempPredict_CrossVal"])
 predict_DF_linearReg_CV = predict_DF_linearReg_CV.join(DF_target)
-predict_DF_linearReg_CV_ChosenDates = predict_DF_linearReg_CV["2012-03-20 12:00:00 ":"2012-03-23 12:00:00"]
+predict_DF_linearReg_CV_ChosenDates = predict_DF_linearReg_CV["2012-03-20 12:00:00 ":"2012-03-26 12:00:00"]
 predict_DF_linearReg_CV_ChosenDates.plot()
 plt.xlabel("time")
 plt.ylabel("temperature")
@@ -167,7 +200,7 @@ plt.title("Cross Validation Prediction")
 plt.show(1)
 
 
-# In[14]:
+# In[ ]:
 
 # finding the metrics related to cross validation 
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
@@ -177,5 +210,30 @@ R2_score_linearReg_CV = r2_score(DF_target,predict_linearReg_CV)
 print 'Coefficient of determination (R-squared) = '+str(R2_score_linearReg_CV)
 
 
+# In[ ]:
+
+# one more prediction model: random forest regressor
+from sklearn.ensemble import RandomForestRegressor
+reg_RF = RandomForestRegressor()
+predict_RF_CV = cross_val_predict(reg_RF,DF_features,DF_target,cv=10)
+predict_DF_RF_CV = pd.DataFrame(predict_RF_CV,index = DF_target.index,columns = ["TempPredict_RandomFor"])
+predict_DF_RF_CV = predict_DF_RF_CV.join(DF_target)
+predict_DF_RF_CV_ChosenDates = predict_DF_RF_CV["2012-03-20 12:00:00 ":"2012-03-26 12:00:00"]
+predict_DF_RF_CV_ChosenDates.plot()
+plt.xlabel("time")
+plt.ylabel("temperature")
+plt.show()
+
+
+# In[ ]:
+
+#finding the metrics related to random forest regressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+mean_absolute_error_linearReg_RF = mean_absolute_error(DF_target,predict_RF_CV)
+mean_squared_error_linearReg_RF = mean_squared_error(DF_target,predict_RF_CV)
+R2_score_linearReg_RF = r2_score(DF_target,predict_RF_CV)
+print 'Coefficient of determination (R-squared) = '+str(R2_score_linearReg_RF)
+
+
 # # Conclusion
-# We can point out that both models turn to be a good approximation of the actual values behaviour. In particular, the cross validation is the model which reaches higher R-Squared showing to be the best fit.
+# We can point out that both models turn to be a good approximation of the actual values behaviour. In particular, the linear regression is the model which reaches higher R-Squared showing to be the best fit.
